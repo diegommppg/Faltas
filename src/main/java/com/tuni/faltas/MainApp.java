43a810ceb9af012e3395108bc4a4e339528651fc
diegommppg <diegommppg@gmail.com>
2024-10-20 12:03:35 +0200

    git commit -m "SCRUM-1 tarea prueba finalizada"

--- src/main/java/com/tuni/faltas/MainApp.java
+++ src/main/java/com/tuni/faltas/MainApp.java
@@ -15,6 +15,7 @@ public class MainApp extends Application {
 
     private static Scene scene;
 
+
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("principal"), 640, 480);
@@ -22,17 +23,21 @@ public class MainApp extends Application {
         stage.show();
     }
 
+
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
+
 
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
+
     public static void main(String[] args) {
         launch();
     }
+
 
 }
