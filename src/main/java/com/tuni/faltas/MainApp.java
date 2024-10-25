326a90481f4a6a39b7bdf314da8ac8c8157ae703
Rodrigo <rodrigocid2002@gmail.com>
2024-10-25 12:06:50 +0200

    SCRUM-47
    Creada interfaz gráfica

--- src/main/java/com/tuni/faltas/MainApp.java
+++ src/main/java/com/tuni/faltas/MainApp.java
@@ -1,43 +1,62 @@
 package com.tuni.faltas;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * JavaFX App
  */
-public class MainApp extends Application {
+public class MainApp extends Application implements Initializable {
 
-    private static Scene scene;
+	private static Scene scene;
 
+	@FXML
+	private ComboBox<String> cbCursos;
+	@FXML
+	private ComboBox<String> cbAsignaturas;
+	
+	private String[] cursos = {"1ºDAM", "1ºDAW", "2ºDAM", "2ºDAW"};
+	
+	@FXML
+	private ComboBox<String> cbAlumnos;
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("principal"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+	@Override
+	public void start(Stage stage) throws IOException {
+		scene = new Scene(loadFXML("principal"), 640, 480);
+		stage.setScene(scene);
+		stage.show();
 
+	}
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+	static void setRoot(String fxml) throws IOException {
+		scene.setRoot(loadFXML(fxml));
+	}
 
+	private static Parent loadFXML(String fxml) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource(fxml + ".fxml"));
+		return fxmlLoader.load();
+	}
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+	public static void main(String[] args) {
+		launch();
+	}
 
-
-    public static void main(String[] args) {
-        launch();
-    }
-
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+			
+		cbCursos.getItems().addAll(cursos);
+		
+	}
 
 }
