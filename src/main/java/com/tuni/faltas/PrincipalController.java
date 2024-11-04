e15af2c68a02cb59e5afa431f019a86b50ff0c72
DavidGlezG <davidglezg5@gmail.com>
2024-11-04 12:42:50 +0100

    Tarea interfaz configuracion realizada

--- src/main/java/com/tuni/faltas/PrincipalController.java
+++ src/main/java/com/tuni/faltas/PrincipalController.java
@@ -5,6 +5,22 @@ import javafx.fxml.FXML;
 
 
 public class PrincipalController {
-
-    
+		
+	//Cambiar a la interfaz cursos
+	@FXML
+	private void switchToCursos() throws IOException {
+		MainApp.setRoot("Cursos");
+	}
+	
+	//Cambiar a la interfaz asignaturas
+		@FXML
+		private void switchToAsignaturas() throws IOException {
+			MainApp.setRoot("Asignaturas");
+		}
+		
+	//Cambiar a la interfaz alumnos
+		@FXML
+		private void switchToAlumnos() throws IOException {
+			MainApp.setRoot("Alumnos");
+		}
 }
