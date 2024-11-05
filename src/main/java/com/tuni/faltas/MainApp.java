d4589fcb7c19430b1a1203ff8c24018f7c0d28da
LEIRE-G <leiregarcianaves@alumnos.tuniversformacion.es>
2024-11-05 12:15:02 +0100

    SCRUM-24 realizado los metodos

--- src/main/java/com/tuni/faltas/MainApp.java
+++ src/main/java/com/tuni/faltas/MainApp.java
@@ -8,6 +8,8 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+
+
 /**
  * JavaFX App
  */
@@ -18,7 +20,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("CursoController"), 640, 480);
+        scene = new Scene(loadFXML("curso"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
@@ -33,8 +35,7 @@ public class MainApp extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
-
-
+    
     public static void main(String[] args) {
         launch();
     }
