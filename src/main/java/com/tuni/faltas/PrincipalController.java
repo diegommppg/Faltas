b09f9458d1122b4b3ab1bc58c406cfae5a697607
DavidGlezG <davidglezg5@gmail.com>
2024-11-05 11:19:38 +0100

    Correción de un error

--- src/main/java/com/tuni/faltas/PrincipalController.java
+++ src/main/java/com/tuni/faltas/PrincipalController.java
@@ -3,24 +3,23 @@ package com.tuni.faltas;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-
 public class PrincipalController {
-		
-	//Cambiar a la interfaz cursos
+
+	// Cambiar a la interfaz cursos
 	@FXML
 	private void switchToCursos() throws IOException {
-		MainApp.setRoot("Cursos");
+		MainApp.setRoot("cursos");
 	}
-	
-	//Cambiar a la interfaz asignaturas
-		@FXML
-		private void switchToAsignaturas() throws IOException {
-			MainApp.setRoot("Asignaturas");
-		}
-		
-	//Cambiar a la interfaz alumnos
-		@FXML
-		private void switchToAlumnos() throws IOException {
-			MainApp.setRoot("Alumnos");
-		}
+
+	// Cambiar a la interfaz asignaturas
+	@FXML
+	private void switchToAsignaturas() throws IOException {
+		MainApp.setRoot("asignaturas");
+	}
+
+	// Cambiar a la interfaz alumnos
+	@FXML
+	private void switchToAlumnos() throws IOException {
+		MainApp.setRoot("alumnos");
+	}
 }
