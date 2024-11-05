4698eeb9f5025ebd184435a290f6e63c82198bab
pabloglezr8 <pabligoru99@gmail.com>
2024-11-05 14:08:51 +0100

    Hacemos la Interfaz de Asignaturas

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.tuni.faltas {
-    requires javafx.controls;
-    requires javafx.fxml;
-	requires com.samuel.PrimerProyectoMaven_DesinT2;
-
-    opens com.tuni.faltas to javafx.fxml;
-    exports com.tuni.faltas;
-}
